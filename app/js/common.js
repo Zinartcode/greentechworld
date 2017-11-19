a9886516d1aa48a3421eb70e021de34ce18b7fa2
Arthur Zinnatullin <arthur@mac.local>
2017-11-19 00:59:46 -0800

    work with APIs

---
+++ app/js/common.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+
+  // NATIONAL GEOGRAPHIC JOURNAL
+
+  var formURL = 'https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=cbdf8301c2814a758a2f6ecfdccdffa8';
+  $.getJSON(formURL, function(api_data) {
+    //  console.log(api_data.articles[0]);
+
+    // console.log(api_data.articles[0].description);
+
+    natgeo1.innerHTML += "<a target=\"_blank\"  href=" + api_data.articles[0].url + "><div class=\"blog-left\" id=\"natgeo_title\"> <h2>" + api_data.articles[0].title + "</h2>"
+    natgeo_title.innerHTML += "<p>" + api_data.articles[0].description + "</p></div></a>"
+
+    natgeo1.innerHTML += "<div class=\"blog-right\"><a target=\"_blank\"  href=" + api_data.articles[0].url + "><img src=" + api_data.articles[0].urlToImage + " class=\"img-responsive\" ></div></a>"
+
+    natgeo2.innerHTML += "<a target=\"_blank\"  href=" + api_data.articles[1].url + "><div class=\"blog-left\" id=\"natgeo_title\"> <h2>" + api_data.articles[1].title + "</h2>"
+    natgeo_title.innerHTML += "<p>" + api_data.articles[1].description + "</p></div></a>"
+
+    natgeo2.innerHTML += "<div class=\"blog-right\"><a target=\"_blank\"  href=" + api_data.articles[1].url + "><img src=" + api_data.articles[1].urlToImage + " class=\"img-responsive\" ></div></a>"
+  });
+
+  // ENGADGET JOURNAL
+
+  var formURL = 'https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=cbdf8301c2814a758a2f6ecfdccdffa8';
+  $.getJSON(formURL, function(api_data) {
+    // console.log(api_data);
+
+    //  console.log(api_data.articles[0]);
+
+    engadget.innerHTML += "<a target=\"_blank\"  href=" + api_data.articles[0].url + "><div class=\"blog-left\" id=\"engadget_title\"> <h2>" + api_data.articles[0].title + "</h2>"
+    engadget_title.innerHTML += "<p>" + api_data.articles[0].description + "</p></div></a>"
+
+    engadget.innerHTML += "<div class=\"blog-right\"><a target=\"_blank\"  href=" + api_data.articles[0].url + "><img src=" + api_data.articles[0].urlToImage + " class=\"img-responsive\" ></div></a>"
+
+  });
+
+  // NATIONAL GEOGRAPHIC INSTAGRAM
+
+  var formURL = 'https://www.instagram.com/natgeo/?__a=1';
+  $.getJSON(formURL, function(api_data) {
+    // console.log(api_data.user.media.nodes[0].display_src);
+
+    var i = 0;
+    api_data.user.media.nodes.forEach(function(key, value) {
+      while (i <= 8) {
+        inst_natgeo.innerHTML += "<div class=\"portfolio-item grid\" style=\"background-image: url(" + api_data.user.media.nodes[i].display_src + ")\"><span class=\"a\">" + api_data.user.username + "</span><span style=\"font-size:16px;\" class=\"b\" hidden>" + api_data.user.media.nodes[i].caption + "</span></div>"
+        // console.log(i);
+        i++;
+
+        $(".portfolio-item").hover(function() {
+          $(this).find('.a').hide();
+          $(this).find('.b').show();
+        }, function() {
+          $(this).find('.b').hide();
+          $(this).find('.a').show();
+        });
+      }
+    });
+  });
+
+  // ENGADGET INSTAGRAM
+
+  var formURL = 'https://www.instagram.com/blonde_in_sf/?__a=1';
+  $.getJSON(formURL, function(api_data) {
+    //  console.log(api_data.user.media.nodes[0].display_src);
+
+    var i = 0;
+    api_data.user.media.nodes.forEach(function(key, value) {
+      while (i <= 8) {
+        inst_leo.innerHTML += "<div class=\"portfolio-item grid\" style=\"background-image: url(" + api_data.user.media.nodes[i].display_src + ")\"><span class=\"a\">" + api_data.user.username + "</span><span style=\"font-size:16px;\" class=\"b\" hidden>" + api_data.user.media.nodes[i].caption + "</span></div>"
+        // console.log(i);
+        i++;
+
+        $(".portfolio-item").hover(function() {
+          $(this).find('.a').hide();
+          $(this).find('.b').show();
+        }, function() {
+          $(this).find('.b').hide();
+          $(this).find('.a').show();
+        });
+      }
+    });
+  });
+
+  var formURL = 'https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=cbdf8301c2814a758a2f6ecfdccdffa8';
+  $.getJSON(formURL, function(api_data) {
+
+    var content = ""
+
+    for (n = 0; n < 6; n++) {
+      content += "<a target=\"_blank\"  href=\"" + api_data.articles[n].url + "\" class=\"blog-item grid\"> \
+                  <span class=\"blog-item-img\" style=\"background-image: url(" + api_data.articles[n].urlToImage + ");\"></span> \
+                    <span class=\"blog-item-content grid\"> \
+                      <h4>" + api_data.articles[n].title + "</h4> \
+                    <p>" + api_data.articles[n].description + "</p> \
+                    </span> \
+                  </a> \
+                    </div>"
+      btm_news.innerHTML = content;
+    }
+  });
+});
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+var gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		concat         = require('gulp-concat'),
+		uglify         = require('gulp-uglify'),
+		cleanCSS       = require('gulp-clean-css'),
+		rename         = require('gulp-rename'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		notify         = require("gulp-notify");
+		// minifyCss      = require('gulp-minify-css'), // Подключаем пакет для минификации CSS
+		rename         = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+		del            = require('del'), // Подключаем библиотеку для удаления файлов и папок
+		imagemin       = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+		pngquant       = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+		// cache          = require('gulp-cache'), // Подключаем библиотеку кеширования
+
+
+// Сервер и автообновление страницы Browsersync
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+	});
+});
+
+// Минификация пользовательских скриптов проекта и JS библиотек в один файл
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js', // Всегда в конце
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Минимизировать весь js (на выбор)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+	return gulp.src('app/css/libs.css') // Выбираем файл для минификации
+		// .pipe(minifyCss()) // Сжимаем
+		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+		.pipe(gulp.dest('app/css')); // Выгружаем в папку app/css
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({outputStyle: 'expand'}).on("error", notify.onError()))
+	.pipe(rename({suffix: '.min', prefix : ''}))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleanCSS()) // Опционально, закомментировать при отладке
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['css-libs', 'js', 'browser-sync'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
+
+gulp.task('scripts', function() {
+	return gulp.src([ // Берем все необходимые библиотеки
+		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		])
+		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+		.pipe(uglify()) // Сжимаем JS файл
+		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+});
+
+
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*') // Берем все изображения из app
+		.pipe(cache(imagemin({ // С кешированием
+		// .pipe(imagemin({  // Сжимаем изображения без кеширования
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('build', ['clean', 'sass', 'scripts'], function() {
+
+	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+		'app/css/main.css',
+		'app/css/libs.min.css'
+		])
+	.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+	.pipe(gulp.dest('dist/fonts'))
+
+	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+	.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+	.pipe(gulp.dest('dist'));
+
+});
